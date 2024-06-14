71d16689cd3837c62de189124431c090deb44b82
michaela-perrotta <michaela.perrotta@hcl.com>
2024-06-14 15:24:43 -0400

    Create buff-overflow.cpp

---
+++ buff-overflow.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main ( int argc, char *argv[] )
+{
+    char stringArray[10] ;
+     
+    printf( "please enter some text" ) ;
+    gets_s( stringArray, sizeof( stringArray ) ) ;
+    fprintf(fp,"%s", buf);
+     
+}
